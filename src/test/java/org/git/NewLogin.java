3cc40c75f9ae5b6c8632b90dd8a2a8b8b7a50736
GayathriMani12 <gayathrigayu505@gmail.com>
2024-04-12 20:19:38 +0530

    Success

---
+++ src/test/java/org/git/NewLogin.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class NewLogin {
+	private void comName() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void compId() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+
+}
